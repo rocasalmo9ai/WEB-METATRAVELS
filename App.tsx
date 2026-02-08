3d3498de8f467ec168425e2b97404e8e2cd98723
rocasalmo9ai <rocasal9ai@gmail.com>
2026-02-08 17:43:26 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,48 +1,33 @@
-import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { Layout } from './components/Layout.tsx';
-import { Home } from './pages/Home.tsx';
-import { PackageDetail } from './pages/PackageDetail.tsx';
-import { WeatherPage } from './pages/WeatherPage.tsx';
-import { Admin } from './pages/Admin.tsx';
-import { CustomTrip } from './pages/CustomTrip.tsx';
-import { PackagesList } from './pages/PackagesList.tsx';
-import { About } from './pages/About.tsx';
-import { Contact } from './pages/Contact.tsx';
-import { LanguageProvider } from './components/LanguageContext.tsx';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [pathname]);
-  return null;
-};
+import { Home } from './pages/Home';
+import { PackagesList } from './pages/PackagesList';
+import { PackageDetail } from './pages/PackageDetail';
+import { CustomTrip } from './pages/CustomTrip';
+import { ThankYou } from './pages/ThankYou';
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
-    <LanguageProvider>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="*" element={
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/packages" element={<PackagesList />} />
-                <Route path="/package/:id" element={<PackageDetail />} />
-                <Route path="/weather" element={<WeatherPage />} />
-                <Route path="/custom-trip" element={<CustomTrip />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/policy" element={<div className="p-20 text-center text-2xl font-serif">Políticas de Privacidad y Términos de Servicio</div>} />
-              </Routes>
-            </Layout>
-          } />
-        </Routes>
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Paquetes */}
+        <Route path="/packages" element={<PackagesList />} />
+        <Route path="/packages/:id" element={<PackageDetail />} />
+
+        {/* Antes era /questionnaire, pero no existe Questionnaire.tsx en tu repo.
+            Usamos CustomTrip (que sí existe) para que compile y no se rompa. */}
+        <Route path="/questionnaire" element={<CustomTrip />} />
+
+        {/* Thank you */}
+        <Route path="/gracias" element={<ThankYou />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
