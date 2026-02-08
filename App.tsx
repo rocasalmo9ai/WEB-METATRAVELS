4f8dfcaf1d298ff696b73d05f30d4f5bf98aad1e
rocasalmo9ai <rocasal9ai@gmail.com>
2026-02-08 16:19:40 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,19 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Packages } from './pages/Packages';
 import { PackageDetail } from './pages/PackageDetail';
+import { Questionnaire } from './pages/Questionnaire';
 import { ThankYou } from './pages/ThankYou';
 
 export const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* Core funnel */}
         <Route path="/" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
         <Route path="/packages/:id" element={<PackageDetail />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
+
         <Route path="/gracias" element={<ThankYou />} />
 
-        {/* fallback */}
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
